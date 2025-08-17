dadb265d92afc01fb65eacbff74762ecc6db9e6a
_01AyushKr <162833196+ayush-kr-ayush@users.noreply.github.com>
2025-08-17 17:09:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'stream-chat-react']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 2000 // optional, warnings kam karne ke liye
+  }
 })
